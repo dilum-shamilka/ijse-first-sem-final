2b2a9696c86b7c49b3a6caa85af85f8da1adbe94
Dilum Shamilka Pahasara <dilumshamilka4@gmail.com>
2025-06-02 21:30:41 +0530

    initial commit

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -5,13 +5,9 @@ module lk.ijse.pahasarastudiofp {
     requires javafx.base;
     requires java.sql;
     requires protobuf.java;
-
-
     requires java.desktop;
     requires java.logging;
     requires java.xml;
-
-
     requires java.mail;
     opens lk.ijse.pahasarastudiofp.controller to javafx.fxml;
     opens lk.ijse.pahasarastudiofp.dto.tm to javafx.base;
