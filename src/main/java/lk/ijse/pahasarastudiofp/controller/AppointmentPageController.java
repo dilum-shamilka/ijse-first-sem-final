4d7a57525741652d472a47357b8ce1810298ea75
Dilum Shamilka Pahasara <dilumshamilka4@gmail.com>
2025-08-05 10:52:25 +0530

    1st

--- src/main/java/lk/ijse/pahasarastudiofp/controller/AppointmentPageController.java
+++ src/main/java/lk/ijse/pahasarastudiofp/controller/AppointmentPageController.java
@@ -22,7 +22,7 @@ import java.util.List;
 import java.util.Optional;
 import java.util.ResourceBundle;
 
-
+// This controller handles the appointment management functionality in the Pahasara Studio application.
 public class AppointmentPageController implements Initializable {
 
     @FXML
